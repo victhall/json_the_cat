d9fc4e1934c5efd11c26acb545872a35fdde759e
“Victoria <victoriahall.dev@gmail.com>
2021-12-02 23:13:55 +0000

    added callback function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,22 @@
+const args = process.argv.slice(2)
 const request = require('request')
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=Siberian';
 
-request(url, (error, response, body) => {
-  if(error) console.log(error)
-  console.log(response.statusCode);
-  console.log(body);
-  const data = JSON.parse(body);
-console.log(data);
-console.log(typeof data);
-for (let item of data) {
-console.log(item.weight)
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + args;
+
+const BreedDesc = (breed, callback) => {
+  request(url, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (error) {
+      return callback(error);
+    }
+    if (breed === null) {
+      console.log('Breed not found.');
+    }
+
+    return callback(data[0].description, '');
+  });
 }
+
+BreedDesc(args, (error, description) => {
+  console.log(error, description)
 });
